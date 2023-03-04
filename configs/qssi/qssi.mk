72c2b08283fc9c656aedb7ef4f6f11fb15f92cc5
chandu078 <chandudyavanapelli03@gmail.com>
2023-03-04 13:23:38 +0100

    audio: config: Use project pathmap
    Change-Id: I92717ba53c5ca714f1701bf6583ef2c90f0b9413
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- configs/qssi/qssi.mk
+++ configs/qssi/qssi.mk
@@ -44,7 +44,7 @@ AUDIO_FEATURE_ENABLED_SVA_MULTI_STAGE := true
 
 ifneq ($(strip $(TARGET_USES_RRO)), true)
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += $(AUDIO_HAL_DIR)/configs/common/overlay
 endif
 
 # Low latency audio buffer size in frames
